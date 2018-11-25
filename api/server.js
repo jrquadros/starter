714a484cf724ae3340d1662c3eee75572d91f90f
jrquadros <quadrosjunioradits@gmail.com>
2018-11-24 23:56:25 -0300

    Conectando o BD

--- api/server.js
+++ api/server.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose'); 
 
+//definindo rota
 const porta = 3001;
 
+//iniciando o DB 
+
+mongoose.connect('mongodb://JrQuadros:batmanDARKSIDE99@ds044587.mlab.com:44587/starter-api', {
+    useNewUrlParser: true,
+});
+
+if(mongoose.connect){
+    console.log('BD conectado');
+}else{
+    console.log('erro ao conectar o BD')
+}
+
+
+
+
+//iniciando o app
 const app = express();
 
 //configurando rota raíz 
