fc296e89827115cd8556d51464e1ef7a645f5f19
jrquadros <quadrosjunioradits@gmail.com>
2018-11-26 02:08:22 -0300

    Reestruturação dos arquivos/ Configuração das rotas/ ProductController criado

--- api/server.js
+++ api/server.js
@@ -7,39 +7,19 @@ const porta = 3001;
 
 //iniciando o DB 
 
-mongoose.connect('mongodb://JrQuadros:batmanDARKSIDE99@ds044587.mlab.com:44587/starter-api', {
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
     useNewUrlParser: true,
 });
 
-if(mongoose.connect){
-    console.log('BD conectado');
-}else{
-    console.log('erro ao conectar o BD');
-}
 
 //faz require automaticamente para todos os models da pasta
 requireDir('./src/models');
 
-//testando incerção no banco de dados
-const Product = mongoose.model('Product');
-
-
 //iniciando o app
 const app = express();
 
-//configurando rota raíz 
-
-app.get('/', (req, res) => {
-
-    Product.create({
-        title: 'React Native',
-        description: 'Build native app with React',
-        url:'http://github.com/facebook/react-native',
-    });
-    
-    return res.send('Hello World');
-});
-
+//Rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(porta);
 
