05604f73c3f97b69f6a9d7d15288d522eaa6b919
jrquadros <quadrosjunioradits@gmail.com>
2018-11-27 13:39:52 -0300

    cors adicionado

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose'); 
 const requireDir = require('require-dir');
 
@@ -10,6 +11,12 @@ const app = express();
 
 //permite usar json para inserir dados
 app.use(express.json());
+
+//permite que outros domínios acessem a api
+app.use(cors(/*Adicionar configurações de segurança
+    ou outros domínios
+    
+    (aberto para todos os domínios)*/));
 
 //iniciando o DB 
 
