3489e9990baa88ac00dd92d4a805dfac50ac6930
jrquadros <quadrosjunioradits@gmail.com>
2018-11-29 00:07:32 -0300

    armazenando no estado

--- web/src/pages/main/index.js
+++ web/src/pages/main/index.js
@@ -4,6 +4,11 @@ import api from '../../services/api';
     
 
 export default class Main extends Component{
+
+    state = {
+        products: []
+    };
+
     //funções do react sempre devem ser utilizadas no modelo de named functions
     componentDidMount(){
         this.loadProducts();
@@ -11,7 +16,9 @@ export default class Main extends Component{
     //funções próprias devem ser utilizadas no modelo de arrow functions
     loadProducts = async () =>{
         const response = await api.get('/products');
-
+        
+        //atribui response à ao estado do componente
+        this.setState({products: response.data.docs});
         console.log(response.data.docs);
 
     };
@@ -19,7 +26,16 @@ export default class Main extends Component{
 
     render(){
         return(
-            <h1>Hello World</h1>
+            <div className="product-list">
+                {this.state.products.map(product => (
+                    //sempre adicionar um id com "key" ao usar o map
+                    <h2 key={product._id}>
+                        {product.title}
+                        <p/>
+                        {product._id}
+                    </h2>
+                ))}
+            </div>
         );
     };
 };
